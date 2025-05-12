1896aecc79fac53897d5e027f15e0f7e7798981d
AK1aNi <22mkiani@heckgrammar.co.uk>
2025-05-12 08:33:21 +0100

    Update Program.cs

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -5,7 +5,23 @@
         static void Main(string[] args)
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-
+    		Console.WriteLine("What fruit would you like to find: ");
+    		string user_fruit = Console.ReadLine();
+    		bool found = false ; 
+    		foreach(string fruit in fruits)
+    		{
+    				if (fruit == user_fruit)
+    				{
+    					found = true;
+    				}
+    		}	
+    		if (found==true)
+    		{
+    			Console.WriteLine("Word is found");
+    		}
+    		else{
+    			Console.WriteLine("Word is not found");
+    		}
             // Your program should:
             // Ask the user what word they would like to find
             // Output the message True if the word is found
